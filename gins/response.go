3e607dd321af7c0e1efe9094466c8cdb2dcad056
lcb <liuchengbin@laoyuegou.com>
2019-07-08 19:27:54 +0800

    //todo:lcb add gins response

---
+++ gins/response.go
@@ -0,0 +1,32 @@
+package gins
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+const (
+	MSG_OK       = 0  // 成功
+	MSG_ERR      = -1 // 错误
+	MSG_REDIRECT = -2 // 重定向
+	MSG_AUTH     = -3 // 登录认证
+)
+
+// Response represents HTTP response body.
+type Response struct {
+	Code int         `json:"code"` // return code, 0 for succ
+	Msg  string      `json:"msg"`  // message
+	Data interface{} `json:"data"` // data object
+}
+
+// NewResponse creates a result with Code=0, Msg="", Data=nil.
+func NewResponse() *Response {
+	return &Response{
+		Code: MSG_OK,
+		Msg:  "",
+		Data: nil,
+	}
+}
+func (this *Response) JSON(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, this)
+}
