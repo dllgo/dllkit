d1d8e74661e23d58cb295bfc7b0597af34b5a4d8
lcb <liuchengbin@laoyuegou.com>
2019-07-15 11:02:23 +0800

    model

--- gins/model.go
+++ gins/model.go
@@ -5,8 +5,8 @@ import (
 )
 
 type Model struct {
-	Id        int64     `gorm:"column:id;primary_key;auto_increment;" json:"id" form:"id"`                     // 主键
-	CreatedAt time.Time `gorm:"column:created_at;type:datetime;not null;" json:"created_at" form:"created_at"` // 创建时间
-	UpdatedAt time.Time `gorm:"column:updated_at;type:datetime;not null;" json:"updated_at" form:"updated_at"` // 更新时间
-	DeletedAt time.Time `sql:"index" json:"-"`                                                                 // 更新时间
+	Id        int64      `gorm:"column:id;primary_key;auto_increment;" json:"id" form:"id"`                     // 主键
+	CreatedAt time.Time  `gorm:"column:created_at;type:datetime;not null;" json:"created_at" form:"created_at"` // 创建时间
+	UpdatedAt time.Time  `gorm:"column:updated_at;type:datetime;not null;" json:"updated_at" form:"updated_at"` // 更新时间
+	DeletedAt *time.Time `sql:"index"`                                                                          // 更新时间
 }
